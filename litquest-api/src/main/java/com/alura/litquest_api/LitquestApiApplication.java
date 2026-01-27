81e2ab337f0841d7b3a93488c1eaf35ac828f164
arielplazasalinas <arielplazasalinas@gmail.com>
2026-01-27 09:49:33 -0300

    Configuracion base de proyecto con spring boot y maven

---
+++ litquest-api/src/main/java/com/alura/litquest_api/LitquestApiApplication.java
@@ -0,0 +1,13 @@
+package com.alura.litquest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitquestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitquestApiApplication.class, args);
+	}
+
+}
---
+++ litquest-api/src/test/java/com/alura/litquest_api/LitquestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.litquest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitquestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
