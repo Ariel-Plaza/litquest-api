f2bdca6cfa0255f23988d20fd212cba4ac68fafa
arielplazasalinas <arielplazasalinas@gmail.com>
2026-02-08 22:41:01 -0300

    Integracion de BD con Java a travez de Spring, Dotenv y configuracion en archivo application.properties, se agrega conexion y prueba en LitquestApiApplication

--- litquest-api/src/main/java/com/alura/litquest_api/LitquestApiApplication.java
+++ litquest-api/src/main/java/com/alura/litquest_api/LitquestApiApplication.java
@@ -1,6 +1,7 @@
 package com.alura.litquest_api;
 
 import com.alura.litquest_api.principal.Principal;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LitquestApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LitquestApiApplication.class, args);
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")  // Busca en la raíz del proyecto
+                .ignoreIfMissing()
+                .load();
+
+        // Imprime para verificar que se cargó (quita después)
+        System.out.println("DB_HOST cargado: " + dotenv.get("DB_HOST"));
+
+        // Cargar como propiedades del sistema
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(LitquestApiApplication.class, args);
 	}
     // metodo que busca spring para arrancar la logica
     @Override
